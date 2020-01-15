2431e778bc8f546ecfadb69357f2c8f0be3973a2
erikacalu <201810099@aluno.fjn.edu.br>
2020-01-15 19:26:00 -0300

    Criado projeto web - react

---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * TRÊS CONCEITOS PARA ENTENDER REACT:
+ *    1. Componente
+ *    2. Estado
+ *    3. Propriedade
+ */
+
+function App() {
+  return (
+    <h1>Hello world</h1>
+  );
+}
+
+export default App;
